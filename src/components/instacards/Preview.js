4216388b5be22edccb75db11c0b374f1a5db024c
= <eren.yener@trendyol.com>
2018-02-17 12:26:24 +0300

    placeholder component added

--- src/components/instacards/Preview.js
+++ src/components/instacards/Preview.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Card, Feed } from 'semantic-ui-react';
-import PlaceHolder from '../placeholders/PlaceHolder';
+import Logo from '../logos/Logo';
+import ReactPlaceholder from 'react-placeholder';
+import "react-placeholder/lib/reactPlaceholder.css";
 
 class Preview extends React.Component {
 
@@ -11,12 +13,21 @@ class Preview extends React.Component {
       <Card fluid>
         <Card.Content>
           <Card.Header>
-             <PlaceHolder name="instagram"></PlaceHolder>
+             <Logo style={{'background-position':'center'}} name="instagram"></Logo>
           </Card.Header>
         </Card.Content>
         <Card.Content>
-
+            <ReactPlaceholder color='#f5f5f5' type='media' rows={3} ready={false}></ReactPlaceholder>
         </Card.Content>
+
+        <ReactPlaceholder color='#f5f5f5' type='rect' ready={false} style={{ 'width': '%100', 'height': '300px', 'margin-right':'0px' }}></ReactPlaceholder>
+
+        <Card.Content>
+            <ReactPlaceholder color='#f5f5f5' type='TextBlock' rows={3} ready={false}></ReactPlaceholder>
+        </Card.Content>
+
+
+
       </Card>
     )
 
---
+++ src/components/logos/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+
+class Logo extends React.Component {
+
+    componentDidMount(){
+        console.log(this.props.name);
+    }
+
+    render () {
+        return (
+            <div style={this.props.style} className={'logo-' + this.props.name + ' ' + 'logo'}></div>
+        );
+    }
+}
+export default Logo
